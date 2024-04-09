87d19b6ee290cd482e1427d6a43bc3c55a57a9e1
Pengpa0012 <peng.ilikha@gmail.com>
2024-04-10 07:58:58 +0800

    added type cursor

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 console.log("Hello World")
+const text = document.querySelector(".text")
+const sample = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor quasi provident a perferendis unde quidem, qui odit omnis est officiis illo, corrupti at blanditiis iusto voluptatibus, nesciunt temporibus? Perspiciatis ratione officiis voluptatem adipisci consequatur esse temporibus obcaecati necessitatibus labore accusantium."
+
+sample.split("").forEach(el => {
+  const span = document.createElement("span")
+  span.textContent = el
+  span.classList = "letter border border-transparent"
+  text.appendChild(span)
+})
+
+const letters = document.querySelectorAll(".letter")
+let cursor = 0
+
+window.addEventListener("keydown", e => {
+  if(e.key == letters[cursor].textContent) {
+    letters[cursor].classList.add("text-white", "bg-green-500")
+    cursor++
+  }
+})
+
+// Menu UI
+//  -Room input
+// Game UI
+//  -User and enemy screen
+// Result modal
+//  -Reset game
+//  -New game
+// Get random paragraphs 
